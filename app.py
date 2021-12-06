66268b50f613028c6a6c8b71f0afd10d12f29f73
Livid <livid@v2ex.com>
2021-12-06 13:08:41 -0800

    Fix IP record split

--- app.py
+++ app.py
@@ -15,7 +15,6 @@ from enum import Enum, IntEnum, unique
 from functools import partial, wraps
 from typing import Any, List, Tuple
 
-import config
 import dns.resolver
 import magic
 import pillow_avif  # noqa
@@ -29,6 +28,7 @@ from resizeimage import resizeimage
 from sentry_sdk import capture_exception, capture_message
 from sentry_sdk.integrations.flask import FlaskIntegration
 
+import config
 from constants import JSON_MIME_TYPE
 
 register_heif_opener()
@@ -162,10 +162,10 @@ def ipip(ip):
         return error(APIError(message="Invalid IPv4 address provided"))
 
     try:
-        ip_metes = ipdb.lookup(ip).split()
+        ip_fields = ipdb.lookup(ip).split("\t")
         # remove a field(`status`) that we assigned.
-        except_ip_meta_length = len(IPRecord._fields) - 1
-        ip_record = IPRecord("ok", True, *ip_metes[:except_ip_meta_length])
+        except_ip_meta_length = len(IPRecord._fields) - 2
+        ip_record = IPRecord("ok", True, *ip_fields[:except_ip_meta_length])
         return success(ip_record._asdict())
     except Exception as e:  # noqa
         capture_exception(e)
