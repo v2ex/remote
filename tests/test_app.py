cfb304ffa6e88d1c5daee9ed5138d7c2f5102657
Livid <livid@v2ex.com>
2021-12-06 13:52:29 -0800

    Add test for images/fit

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,4 +1,9 @@
+import base64
+import io
+import json
+
 import pytest
+from PIL import Image
 
 from app import app
 
@@ -22,3 +27,22 @@ def test_ping(client):
 def test_dns_resolve(client):
     response = client.get("/dns/resolve?domain=example.com")
     assert response.status_code == 200
+
+
+def test_images_fit_320(client):
+    data = {}
+    with open("tests/hello.png", "rb") as image_file:
+        data["file"] = (image_file, "hello.png")
+        response = client.post(
+            "/images/fit/320",
+            data=data,
+            follow_redirects=True,
+            content_type="multipart/form-data",
+        )
+        assert response.status_code == 200
+        assert b"output" in response.data
+
+        o = json.loads(response.data)
+
+        im = Image.open(io.BytesIO(base64.b64decode(o["output"])))
+        assert im.size == (320, 320)
