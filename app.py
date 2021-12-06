8e2f409534ddbc7f3187a3ac76d9c52ec7650b40
fishermanadg <fishermanadg@gmail.com>
2021-12-07 04:55:59 +0800

    fix bugs: check uploaded file

--- app.py
+++ app.py
@@ -15,7 +15,6 @@ from enum import Enum, IntEnum, unique
 from functools import partial, wraps
 from typing import Any, List, Tuple
 
-import config
 import dns.resolver
 import magic
 import pillow_avif  # noqa
@@ -29,6 +28,7 @@ from resizeimage import resizeimage
 from sentry_sdk import capture_exception, capture_message
 from sentry_sdk.integrations.flask import FlaskIntegration
 
+import config
 from constants import JSON_MIME_TYPE
 
 register_heif_opener()
@@ -265,7 +265,7 @@ class SupportedImageTypes(Enum):
 @app.route("/images/prepare_jpeg", methods=["GET", "POST"])
 def prepare_jpeg():
     # check uploaded file is valid or not
-    if _uploaded := request.files.get("file"):
+    if not (_uploaded := request.files.get("file")):
         return error(APIError(message="No file was uploaded"))
     uploaded = _uploaded.read()
     if not (mime := _get_mime(uploaded)):
@@ -332,7 +332,7 @@ def prepare_jpeg():
 @app.route("/images/fit/<int:box>", methods=["GET", "POST"])
 def fit(box: int):
     # check uploaded file is valid or not
-    if _uploaded := request.files.get("file"):
+    if not (_uploaded := request.files.get("file")):
         return error(APIError(message="No file was uploaded"))
     uploaded = _uploaded.read()
     if not (mime := _get_mime(uploaded)):
@@ -393,7 +393,7 @@ class AvatarSize(IntEnum):
 @app.route("/images/resize_avatar", methods=["GET", "POST"])
 def resize_avatar():
     # check uploaded file is valid or not
-    if _uploaded := request.files.get("file"):
+    if not (_uploaded := request.files.get("file")):
         return error(APIError(message="No file was uploaded"))
     uploaded = _uploaded.read()
     if not (mime := _get_mime(uploaded)):
