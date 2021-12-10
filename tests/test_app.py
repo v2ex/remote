c600822beee1e5c7b93dc692b85e7cd98fd31f9c
fishermanadg <fishermanadg@gmail.com>
2021-12-10 23:33:21 +0800

    Fix load config in case without set APP_ENV

--- tests/test_app.py
+++ tests/test_app.py
@@ -3,7 +3,7 @@ import os
 import unittest
 from typing import Any, Dict
 
-from remote.app import AppENV, create_app
+from remote.app import APP_ENV_NAME, AppENV, create_app
 
 
 class TestBase(unittest.TestCase):
@@ -42,3 +42,22 @@ class TestApp(TestBase):
                 self.assertEqual(app_2.config[k], v)
             else:
                 self.assertNotEqual(app_2.config[k.upper()], v)
+
+    def test_config_from_env_without_app_env(self):
+        # Confirm the `APP_ENV` env is not set, otherwise the app will use it.
+        os.unsetenv(APP_ENV_NAME)
+
+        test_env_configs: Dict[str, Any] = {
+            "UID": "test_uid_2",
+            "DEBUG": json.dumps(True),
+            "region": "test_region_2",
+        }
+        os.environ.update(test_env_configs)
+
+        app_3 = create_app(name="test3")
+
+        for k, v in test_env_configs.items():
+            if k.isupper():
+                self.assertEqual(app_3.config[k], v)
+            else:
+                self.assertNotEqual(app_3.config[k.upper()], v)
